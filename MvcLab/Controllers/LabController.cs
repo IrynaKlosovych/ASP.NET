5e4eccefdaa887c06ba93d313929ce7fc59cfa41
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2025-10-29 13:59:56 +0200

    lab 1 task 3

---
+++ MvcLab/Controllers/LabController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcLab.Controllers
+{
+    public class LabController : Controller
+    {
+        public IActionResult Info()
+        {
+            var model = new
+            {
+                Number = 1,
+                Topic = "Вступ до ASP.NET Core\r\n",
+                Goal = "ознайомитися з основними принципами роботи .NET, " +
+                "навчитися налаштовувати середовище розробки та встановлювати необхідні компоненти, " +
+                "набути навичок створення рішень та проектів різних типів, набути навичок обробки запитів " +
+                "з використанням middleware.\r\n",
+                Author = "Ірина Клосович"
+            };
+            return View(model);
+        }
+    }
+}
