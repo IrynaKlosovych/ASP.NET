f67cb84fec812c9d5095cd5d0a3c59d0024ee4c0
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2025-10-29 13:56:16 +0200

    lab 1 task 1

---
+++ ConsoleToWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Це вебзастосунок, що створений з консольного.");
+
+        app.Run();
+    }
+}
